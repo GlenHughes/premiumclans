804f7719f23c67d7550f36fa01aa7037e15ea5fe
GlenHughes <glen.866@gmailcom>
2015-10-15 18:22:58 +0100

    Updated

---
+++ module/Album/src/Album/Model/Album.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Album\Model;
+
+class Album {
+
+	public $id;
+	public $title;
+	public $artist;
+
+	public function exchangeArray($data) {
+
+		$this->id = (!empty($data['id'])) ? $data['id'] : null;
+		$this->title = (!empty($data['title'])) ? $data['title'] : null;
+		$this->artist = (!empty($data['artist'])) ? $data['artist'] : null;
+
+	}
+
+}
---
+++ module/Album/src/Album/Model/AlbumTable.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Album\Model;
+
+use Zend\Db\TableGateway\TableGateway;
+
+class AlbumTable {
+	
+	protected $tableGateway;
+
+	public function __constructor(TableGateway $tableGateway) {
+		$this->tableGateway = $tableGateway;
+	}
+
+	public function fetchAll() {
+		return $this->tableGateway->select();
+	}
+
+	public function getAlbum($id) {
+		$id = (int) $id;
+		$rowSet = $this->tableGateway->select(array('id' => $id));
+		$row = $rowSet->current();
+		if (!$row) {
+			throw new \Exception('Unable to find '.$id);
+		}
+		return $row;
+	}
+
+	public function saveAlbum (Album $album) {
+		$data = [
+			'artist' => $album->artist,
+			'title' => $album->title
+		];
+
+		$id = (int) $album->id;
+		if ($id === 0) {
+			$this->tableGateway->insert($data);
+		}
+		else {
+			if ($this->getAlbum($id)) {
+				$this->tableGateway->update($data);
+			}
+			else {
+				throw new \Exception('Album does not exists!');
+			}
+		}
+	}
+
+	public function deleteAlbum($id) {
+		$this->tableGateway->delete(array('id' => (int) $id));
+	}
+
+}
